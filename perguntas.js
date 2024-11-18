c2b69004a939475b8609c35fb33c94025405b919
someoneinBrazil <00001108861726sp@al.educacao.sp.gov.br>
2024-11-18 18:05:12 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Mitologia',
+    'Quem é Píton?',
+    'Píton, na mitologia grega, é uma serpente gigantesca, que nasceu do lodo na Terra após o grande dilúvio.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Mitologia',
+    'Quem é Apófis?',
+    'Na mitologia egípcia, Apepe ou Apófis em grego era a personificação egípcia do caos e por isso era o oponente da luz e de Ma at (personificação da ordem/verdade) e conhecido como "Senhor do Caos"'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Mitologia',
+    'Quem é Jörmungandr?',
+    'Na mitoliga nórdica, Jormungand, também chamado de Jörmundgander, é uma criatura monstruosa em forma de cobra gigante. Era conhecida com "A Serpente do Mundo", pelo fato de quando ela se enrolava mordendo a própria cauda, podia dar a volta completa no mundo, que no caso era chamado de Midgard, na cosmologia nórdica.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Mitologia',
+    'E no Brasil? Qual nossa cobra mitológica?',
+    'Na mitologia brasileira, a cobra é um símbolo central em diversas lendas e folclores. A cobra-grande, por exemplo, é vista como um ser sagrado, controlador das águas e protetora das matas. A lenda da cobra grande explica a origem dos sulcos onde correm os rios, que seriam os rastros por onde a cobra havia passado'
 )
